b0a5b4603382d1eb8bfad0d7a07e6b66fa017ab2
nzepeda <NestorDZepeda@gmail.com>
2016-10-08 14:03:20 -0700

    Initial Commit

---
+++ ProjectAlpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectAlpha
+//
+//  Created by Nestor Zepeda on 10/8/16.
+//  Copyright © 2016 Alpha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
