9e94a08598df614ce71ccc63fbb359e9259a109c
nzepeda <NestorDZepeda@gmail.com>
2016-10-26 23:42:29 -0700

    UI updates to the Categories View

--- ProjectAlpha/CategoryTableViewController.swift
+++ ProjectAlpha/CategoryTableViewController.swift
@@ -15,11 +15,24 @@ class CategoryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let c1 = Category(categoryTitle: "Penny Boards", categoryImage: UIImage(named: "PennyBoard.png")!);
-        let c2 = Category(categoryTitle: "Skateboards", categoryImage: UIImage(named: "PennyBoard.png")!);
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.none;
+        
+        insertData();
+    }
+    
+    fileprivate func insertData(){
+        
+        let c1 = Category(categoryTitle: "Camping Tents", categoryImage: UIImage(named: "CampingTent.png")!);
+        let c2 = Category(categoryTitle: "Carry On Suitcases", categoryImage: UIImage(named: "CarryOnSuitcase.png")!);
+        let c3 = Category(categoryTitle: "Penny Boards", categoryImage: UIImage(named: "PennyBoard.png")!);
+        let c4 = Category(categoryTitle: "Road Bikes", categoryImage: UIImage(named: "RoadBike.png")!);
+        let c5 = Category(categoryTitle: "Video Games", categoryImage: UIImage(named: "VideoGames.png")!);
         
         categories.append(c1);
         categories.append(c2);
+        categories.append(c3);
+        categories.append(c4);
+        categories.append(c5);
     }
 
     override func didReceiveMemoryWarning() {
